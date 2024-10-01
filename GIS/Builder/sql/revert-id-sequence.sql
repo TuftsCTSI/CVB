929720e5c890109e2eea2feed96cd84682c1ab2d
jshoughtaling <jared.houghtaling@tuftsmedicalcenter.org>
2024-10-01 06:01:32 -0400

    Fix ID assignment sequence for GIS

--- GIS/Builder/sql/revert-id-sequence.sql
+++ GIS/Builder/sql/revert-id-sequence.sql
@@ -3,13 +3,13 @@ DROP SEQUENCE IF EXISTS vocab.master_id_assignment;
 -- Note - I usually give a small buffer (100 uids or so) for admin/special custom ids
 CREATE SEQUENCE vocab.master_id_assignment
     INCREMENT -1
-    MINVALUE 2147000000
-    MAXVALUE 2147483647
-    START 2147483546
+    MINVALUE 2052000000
+    MAXVALUE 2052500000
+    START 2052499885
     OWNED BY vocab.concept.concept_id;
 
 SELECT setval('vocab.master_id_assignment',
-              (SELECT COALESCE(min(concept_id), 2147483547)
+              (SELECT COALESCE(min(concept_id), 2052499886)
                FROM vocab.concept
-               WHERE concept_id > 2147000000
+               WHERE concept_id > 2052000000 AND concept_id < 2052500000
                  AND standard_concept = 'S'));
