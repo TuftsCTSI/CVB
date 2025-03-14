7885f72bce2f8581b043be70aacca21da9f4338a
jshoughtaling <jared.houghtaling@tuftsmedicalcenter.org>
2025-03-14 15:30:26 +0100

    Add ci/cd for MIMIC vocab

--- MIMIC/Builder/revert-db.sh
+++ MIMIC/Builder/revert-db.sh
@@ -5,10 +5,10 @@ export SQL_DIRECTORY="${RUNNER_WORKDIR}/CVB/CVB/MIMIC/Builder/sql"
 export MAP_DIRECTORY="${RUNNER_WORKDIR}/CVB/CVB/MIMIC/Mappings"
 
 # Remove all additions from db
-psql -h "$2" -d gis_vocabulary -U postgres -f "${SQL_DIRECTORY}/hard-reset.sql"
+psql -h "$2" -d mimic_vocabulary -U postgres -f "${SQL_DIRECTORY}/hard-reset.sql"
 
 # Remove all additions from db
-psql -h "$2" -d gis_vocabulary -U postgres -f "${SQL_DIRECTORY}/create-general-concepts.sql"
+psql -h "$2" -d mimic_vocabulary -U postgres -f "${SQL_DIRECTORY}/create-general-concepts.sql"
 
 # Revert id sequence
-psql -h "$2" -d gis_vocabulary -U postgres -f "${SQL_DIRECTORY}/revert-id-sequence.sql"
+psql -h "$2" -d mimic_vocabulary -U postgres -f "${SQL_DIRECTORY}/revert-id-sequence.sql"
